f944d3870fe2c213fd8c79eb5c48be1c3268ffef
Rix34 <r.skoczek34@gmail.com>
2023-01-22 23:01:49 +0100

    Add Rotation to player and Camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,14 @@ public class PlayerController : MonoBehaviour
    [SerializeField] private float playerSpeed = 2.0f;
    [SerializeField] private float jumpHeight = 1.0f;
    [SerializeField] private float gravityValue = -9.81f;
+   [SerializeField] private float rotationSpeed = 10f;
 
     // References
     private CharacterController controller;
     private PlayerInput playerInput;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
+    private Transform cameraTransform;
 
     // InputSystem Actions
     private InputAction moveAction;
@@ -31,7 +33,7 @@ public class PlayerController : MonoBehaviour
         //Get Components to controll them
         controller = GetComponent<CharacterController>();
         playerInput= GetComponent<PlayerInput>();
-
+        cameraTransform= Camera.main.transform;
         // Match Input with simplified name
         moveAction = playerInput.actions["Move"];
         jumpAction = playerInput.actions["Jump"];
@@ -49,6 +51,13 @@ public class PlayerController : MonoBehaviour
         Vector2 input = moveAction.ReadValue<Vector2>();
         // Change Direction to match input
         Vector3 move = new Vector3(input.x, 0 , input.y);
+
+        // Move in Direction of camera
+        move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
+
+        // To be sure that Y doesnt get any weird value
+        move.y = 0f;
+
         // Move character
         controller.Move(move * Time.deltaTime * playerSpeed);
 
@@ -61,5 +70,12 @@ public class PlayerController : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+        // Rotate player to rotation of Camera
+        float targetAngle = cameraTransform.eulerAngles.y;
+        
+        Quaternion targetRotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
