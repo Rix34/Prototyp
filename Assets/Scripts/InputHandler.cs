ab283263dd5fbe4be21ac9c79a0bb02cf9273383
mszczechowicz <scela0718@gmail.com>
2023-01-24 20:58:59 +0100

    Poprawki sceny
    Dodanie pustego skryptu InputHandler.
    Zmienienie barw sceny na mniej kłujące w oczy.

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+	public float move;
+
+	PlayerControls playercontrol;
+}
